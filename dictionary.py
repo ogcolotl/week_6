2069e9facdf8aa9e6aa93cbd5b42b124129f9eae
Colotl-Garista <ocolotl-gar@cps.k12.il.us>
2024-10-02 12:03:35 -0500

    made a new folder

---
+++ dictionary.py
@@ -0,0 +1,39 @@
+# dictionary = a collection of {key:value} pairs ordered and changeable. No duplicates
+
+capitals = {"USA": "Washington DC", "India": "New Delhi", "China": "Beijing", "Russia": "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("USA"))
+
+# if capitals.get("Japan"):
+#     print("The capital exist")
+# else:
+#     print("That capital doesnt exist")
+
+# capitals.update({"Germany": "Berlin"})
+
+# capitals.update({"Spain": "Madrid"})
+
+# print(capitals)
+
+# capitals.pop("China")
+# capitals.popitem()
+# capitals.clear()
+
+# keys = capitals.keys()
+
+# print(keys)
+
+# vales = capitals.values
+
+# for key in capitals.keys():
+# print(key)
+
+values = capitals.values()
+for value in capitals.vales()
+print(value)
+
+items = capitals.items()
+for key, value in capitals.items():
+    print(f"{key}: {value}")
